1070ad718422e1077140a82ef4bb3d7a18397e47
Mamadou-shop <Gneidmamadou17@gmail.com>
2026-04-12 15:11:12 +0000

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const catalogueDouxDoux = [
     { id: 20, titre: "Ampoule LED Connectée", prix: "9.000", cat: "Maison", img: "https://i.pinimg.com/736x/6f/f3/6b/6ff36b1029308d3590d52060f21c188f.jpg" },
     { id: 21, titre: "Parfum Homme 50ml", prix: "12.000", cat: "Beauté", img: "https://i.pinimg.com/736x/ac/6e/7c/ac6e7c34bb818963af1d1bce9752a19f.jpg" },
     { id: 22, titre: "Crème Hydratante", prix: "3.500", cat: "Beauté", img: "https://i.pinimg.com/736x/8e/bc/55/8ebc5596743eb7221c2d1878c3d4492b.jpg" },
-    { id: 23, titre: "Baskets Urbaines", prix: "18.500", cat: "Mode", img: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=500" },
+    { id: 23, titre: "Baskets Urbaines", prix: "18.500", cat: "Mode", img: "https://i.pinimg.com/736x/f1/eb/a2/f1eba2da56e538a8b38ed46193bb7648.jpg" },
     { id: 24, titre: "Adaptateur Secteur", prix: "2.500", cat: "Accessoires", img: "https://via.placeholder.com/280" },
     { id: 25, titre: "Gourde Isotherme", prix: "6.000", cat: "Maison", img: "https://via.placeholder.com/280" },
     { id: 26, titre: "Tapis de souris XXL", prix: "5.500", cat: "Informatique", img: "https://via.placeholder.com/280" },
